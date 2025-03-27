2544744df68d2ddc966a4a7970149d2793753ed4
1nflutorm <1nflutorm@mail.ru>
2025-03-27 15:24:09 +0300

    done

--- app/src/main/java/com/example/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.kt
@@ -4,20 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import com.example.firstandroidapp.ui.theme.FirstAndroidAppTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +38,166 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        message = "С днём рождения, Иван",
-                        from = "от бутылки виски",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(red = 0.3f, green = 0.3f, blue = 0.3f)
+                ) {
+                    MyCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyCard() {
+    Box(
+        modifier = Modifier.fillMaxSize() // Занимает весь экран
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(start = 10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val image = painterResource(R.drawable.cpp)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+            )
+            Text(
+                text = stringResource(R.string.my_name),
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp
+            )
+            Text(
+                text = stringResource(R.string.cpp_developer),
+                fontSize = 20.sp
+            )
+
+//        Text(
+//            text = "Телефон: +79213591464",
+//            modifier = Modifier.padding(top = 100.dp, bottom = 8.dp),
+//        )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp) // Добавим небольшой отступ от краев экрана
+        ) {
+
+            // Основной контейнер для двух колонок
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter) // Выравнивание по нижнему центру
+                    .fillMaxWidth(), // Заполняет всю ширину экрана
+                horizontalArrangement = Arrangement.Center, // Центрируем все содержимое в строке
+            ) {
+                // Первая колонка с иконками
+                Column(
+                    modifier = Modifier
+                        .weight(0.8f)// Занимает 50% ширины экрана
+                        .align(Alignment.CenterVertically) // Выравниваем по центру по вертикали
+                        .padding(end = 16.dp) // Отступ справа
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        val image = painterResource(R.drawable.phone)
+                        Image(
+                            painter = image,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(25.dp) // Устанавливаем размер изображения
+                                .padding(end = 5.dp) // Отступ между картинкой и текстом
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        val image = painterResource(R.drawable.telegram)
+                        Image(
+                            painter = image,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(25.dp)
+                                .padding(end = 5.dp)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        val image = painterResource(R.drawable.mail)
+                        Image(
+                            painter = image,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(25.dp)
+                                .padding(end = 5.dp)
+                        )
+                    }
+                }
+
+                // Вторая колонка с текстами
+                Column(
+                    modifier = Modifier
+                        .weight(1.2f) // Занимает 50% ширины экрана
+                        .align(Alignment.CenterVertically) // Центрируем по вертикали
+                        //.padding(start = 16.dp) // Отступ слева
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start // Центрируем текст
+                    ) {
+                        Text(
+                            text = stringResource(R.string.phone_text),
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        Text(
+                            text = stringResource(R.string.tg_text),
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        Text(
+                            text = stringResource(R.string.mail_text),
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp
+                        )
+                    }
                 }
             }
         }
@@ -45,16 +212,18 @@ fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
     ) {
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 70.sp,
             lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = Color.White
         )
         Text(
             text = from,
             fontSize = 36.sp,
+            color = Color.White,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -64,6 +233,26 @@ fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FirstAndroidAppTheme {
-        Greeting("С днём рождения, Иван", "от бутылки виски")
+        MyCard()
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.fon);
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Greeting(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
